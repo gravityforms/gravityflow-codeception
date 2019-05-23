d8755a3f3fa49f1fafcbf31caf1a047dd425e8ad
Steve Henty <stevehenty@gmail.com>
2019-05-23 09:42:08 +0200

    Cache Gravity Forms and Flow by default

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,35 @@ wp-su() {
     sudo -E -u www-data wp "$@"
 }
 
+install_gravity_forms() {
+    if [ -z ${GITHUB_TOKEN} ]; then
+
+        echo "Installing the latest version of Gravity Forms using the CLI"
+
+        wp-su plugin install gravityformscli --force --activate
+        wp-su gf install --key=${GF_KEY} --activate --force --quiet
+        echo "Gravity Forms installation complete"
+        wp-su gf tool verify-checksums
+
+    else
+
+        rm -rf /wp-core/wp-content/plugins/gravityforms
+
+        echo "Grabbing the latest development master of Gravity Forms"
+
+        git clone -b master --single-branch https://$GITHUB_TOKEN@github.com/gravityforms/gravityforms.git /wp-core/wp-content/plugins/gravityforms
+
+    fi
+}
+
+install_gravity_flow() {
+    rm -rf /wp-core/wp-content/plugins/gravityflow
+
+    echo "Grabbing the latest development master of Gravity Flow"
+
+    git clone -b master --single-branch https://github.com/gravityflow/gravityflow.git /wp-core/wp-content/plugins/gravityflow
+}
+
 # Clean up from previous tests
 rm -rf /wp-core/wp-content/uploads/gravity_forms
 
@@ -36,32 +65,19 @@ if ! $(wp-su core is-installed); then
     # The development version of Gravity Flow requires SCRIPT_DEBUG
     wp-su core config --dbhost=mysql --dbname=wordpress --dbuser=root --dbpass=wordpress --extra-php="define( 'SCRIPT_DEBUG', true );" --force
 
-fi
-
-if [ -z ${GITHUB_TOKEN} ]; then
-
-    echo "Installing the latest version of Gravity Forms using the CLI"
-
-    wp-su plugin install gravityformscli --force --activate
-    wp-su gf install --key=${GF_KEY} --activate --force --quiet
-    echo "Gravity Forms installation complete"
-    wp-su gf tool verify-checksums
-
+    install_gravity_forms
+    install_gravity_flow
 else
 
-    rm -rf /wp-core/wp-content/plugins/gravityforms
+    if [ -z ${NO_CACHE_GRAVITY_FORMS} ]; then
+        install_gravity_forms
+    fi
 
-    echo "Grabbing the latest development master of Gravity Forms"
-
-    git clone -b master --single-branch https://$GITHUB_TOKEN@github.com/gravityforms/gravityforms.git /wp-core/wp-content/plugins/gravityforms
-
+    if [ -z ${NO_CACHE_GRAVITY_FLOW} ]; then
+        install_gravity_flow
+    fi
 fi
 
-rm -rf /wp-core/wp-content/plugins/gravityflow
-
-echo "Grabbing the latest development master of Gravity Flow"
-
-git clone -b master --single-branch https://github.com/gravityflow/gravityflow.git /wp-core/wp-content/plugins/gravityflow
 
 cd /project
 
